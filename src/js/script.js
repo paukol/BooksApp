f9eef7a0062f83678df6192b588d18f686d442b3
Paulina Kolonczyk <paulinakolonczyk@interia.pl>
2021-03-27 16:25:13 +0100

    Add initActions 10.2.2

--- src/js/script.js
+++ src/js/script.js
@@ -1,34 +1,56 @@
 {
-    'use strict';
+  'use strict';
   
-    const select = {
-      templateOf: {
-        templateBook: '#template-book'
-      },
-      containerOf: {
-        booksList: '.books-list'
-      }
-    };
-    
-    const templates = {
-        booksList: Handlebars.compile(document.querySelector(select.templateOf.templateBook).innerHTML),
-      };
-    
-    const render = function() {
-    const thisBooksList = this; 
+  const select = {
+    templateOf: {
+      templateBook: '#template-book'
+    },
+    containerOf: {
+      booksList: '.books-list',
+      bookImage: '.book__image',
+    },
+    class: {
+      favourite: 'favorite',
+    },
+  };  
 
+  const templates = {
+    booksList: Handlebars.compile(document.querySelector(select.templateOf.templateBook).innerHTML),
+  };
 
+  const templateOfBooks = [];
+  const favoriteBook = [];
+
+  const render = function() {
+   
     for(let book of dataSource.books){
-      
-          const generatedHTML = templates.booksList(book);
-          thisBooksList.element = utils.createDOMFromHTML(generatedHTML);
-          const booksContainer = document.querySelector(select.containerOf.booksList);
-          booksContainer.appendChild(thisBooksList.element);
-      }
+ 
+      const generatedHTML = templates.booksList(book);
+      const generatedDOM = utils.createDOMFromHTML(generatedHTML);
+      const booksContainer = document.querySelector(select.containerOf.booksList);
+      booksContainer.appendChild(generatedDOM);
+      templateOfBooks.push(generatedDOM);
     }
-    render();
+  };
 
+  const initActions = function(){
     
+    for(let book of templateOfBooks){
+      let covers = book.querySelector('.book__image');
+      console.log('covers:', covers);
+      
+      covers.addEventListener('dblclick', function(event){
+        event.preventDefault();
+        covers.classList.add(select.class.favourite);
+        const id = covers.getAttribute('data-id');
+        favoriteBook.push(id);    
+      });
+    }
+  }
+   
+  render();
+  initActions();
+  console.log('favoriteBook:', favoriteBook);
+
+  
 }
-
-
